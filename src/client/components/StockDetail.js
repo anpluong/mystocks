37cfd7a09e8ed2dd4bdea872292f16d86b3a159c
anpluong <thinkingtea@gmail.com>
2018-05-09 19:54:13 -0700

    add the stock price and manipulate the stock object

--- src/client/components/StockDetail.js
+++ src/client/components/StockDetail.js
@@ -1,6 +1,9 @@
 import React, {Component} from 'react';
 import axios from 'axios';
+import StockDateList from './StockDateList';
 
+import { VictoryBar, VictoryChart, VictoryAxis, VictoryPie,
+    VictoryTheme, VictoryLabel, VictoryLine, VictoryLegend, VictoryStack } from 'victory';
 const apiKey = '6H8OCBWU5LYNFJOH';
 const URL = `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&apikey=${apiKey}&symbol=`;
 
@@ -15,35 +18,39 @@ class StockDetail extends Component {
         this.state = {
             stockLabel: '',
             stockDate: '',
-            stockPrice: ''
+            stockPriceOpen: '',
+            stockPriceClose: '',
+            priceHistory: null
         }      
     }
 
-    // // a function to make axios to get the stock
-    //  componentDidMount() {        
-    //         axios.get(URL + "FB")         
-    //         .then((response) => {         
-                
-    //         var stockDate = response.data["Meta Data"]["3. Last Refreshed"];
-    //         //     // var newDate = stockDate.substr(0, stockDate.indexOf(' '));            
-    //         this.setState({
-    //             stockS: stockDate
-    //         }, () => {
-    //             console.log(stockDate)
-    //         })            
-    // //    })
-    //    })
-    // }
-
     componentWillReceiveProps(nextProps) {
         axios.get(URL + nextProps.selectedStock)         
-        .then((response) => {         
-            var stockDate = response.data["Meta Data"]["3. Last Refreshed"];
-            var stockPrice = response.data["Time Series (Daily)"][stockDate]["4. close"];
+        .then((response) => {     
+            const timeSeries = "Time Series (Daily)";
+            let dateObject = response.data[timeSeries]
+            let stockDate = response.data["Meta Data"]["3. Last Refreshed"].split(" ");
+            let stockPriceOpen = dateObject[stockDate[0]]["1. open"];
+            let stockPriceClose = dateObject[stockDate[0]]["4. close"];
+            // let newArray = [];
+
+            //console.log(dateObject)         
+
+            // for (var property in dateObject) {                
+            //     newArray.push({
+            //         [property]: Number(dateObject[property]["1. open"])
+            //     });
+            // }
+            // // this is the array of all the data open stock price in 30 days
+            // newArray = newArray.slice(0, 7);
+            // // console.log(newArray)            
+
             this.setState({
             stockLabel: nextProps.selectedStock,
             stockDate,
-            stockPrice
+            stockPriceClose,
+            stockPriceOpen,
+            priceHistory: dateObject
         })
     }) 
     }
@@ -62,7 +69,7 @@ class StockDetail extends Component {
                             <th>Stock Symbol</th>
                             <th>Date</th>
                             <th>Open Price </th>
-                            {/*<th>Close Price </th> */}
+                            <th>Close Price </th>
                         </tr>
                     </thead>
                     <tbody>                      
@@ -74,12 +81,18 @@ class StockDetail extends Component {
                                 {this.state.stockDate}
                             </td>
                             <td>
-                                {this.state.stockPrice}
+                                {this.state.stockPriceOpen}
                             </td>
+                            <td>
+                                {this.state.stockPriceClose}
+                            </td>                            
                         </tr>
-                    </tbody>
+                    </tbody>                    
                 </table>
-            </div>
+                <div>
+                    <StockDateList pH = {this.state.priceHistory} />
+                </div>
+            </div>            
         )
     }
 }
